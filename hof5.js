6270fcc4be50df2e8ed5cd93cf8885a45f5fe1bb
Blade Boles <bladerboles@gmail.com>
2019-12-02 20:43:46 -0700

    Really messy, but I got it to work :-)

---
+++ hof5.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let inputPhrase = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+
+let inputArray = inputPhrase.split(' ');
+
+function reduce(arr) {
+  let answer = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length === 3) {
+      answer = answer.concat(` `);
+    } else {
+      let splitWord = arr[i].split('');
+      answer = answer.concat(splitWord[splitWord.length - 1].toUpperCase());
+      
+    }
+  }
+  return answer;
+}
+
+console.log(reduce(inputArray));
+
